93b0a8592f66dbf6e8f801a4709b29232c0038c9
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2019-04-05 08:58:04 +0000

    add config, networks

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -476,15 +476,75 @@ contract ERC721 is IERC721 {
 	}
 }
 
-contract MyERC721 is ERC721 {
-	constructor () public { }
+contract MyTRC721 is ERC721 {
+    // Token name
+    string private _name;
 
-	function mintUniqueTokenTo(
-		address _to,
-		uint256 _tokenId
-	) public
-	{
-		super._mint(_to, _tokenId);
-	}
+    // Token symbol
+    string private _symbol;
+
+    // Optional mapping for token URIs
+    mapping(uint256 => string) private _tokenURIs;
+
+    /**
+     * @dev Constructor function
+     */
+    constructor (string memory name, string memory symbol) public {
+        _name = name;
+        _symbol = symbol;
+    }
+
+    /**
+     * @dev Gets the token name.
+     * @return string representing the token name
+     */
+    function name() external view returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * @dev Gets the token symbol.
+     * @return string representing the token symbol
+     */
+    function symbol() external view returns (string memory) {
+        return _symbol;
+    }
+
+    /**
+     * @dev Returns an URI for a given token ID.
+     * Throws if the token ID does not exist. May return an empty string.
+     * @param tokenId uint256 ID of the token to query
+     */
+    function tokenURI(uint256 tokenId) external view returns (string memory) {
+        require(_exists(tokenId));
+        return _tokenURIs[tokenId];
+    }
+
+    /**
+     * @dev Internal function to set the token URI for a given token.
+     * Reverts if the token ID does not exist.
+     * @param tokenId uint256 ID of the token to set its URI
+     * @param uri string URI to assign
+     */
+    function _setTokenURI(uint256 tokenId, string memory uri) internal {
+        require(_exists(tokenId));
+        _tokenURIs[tokenId] = uri;
+    }
+
+    /**
+     * @dev Internal function to burn a specific token.
+     * Reverts if the token does not exist.
+     * Deprecated, use _burn(uint256) instead.
+     * @param owner owner of the token to burn
+     * @param tokenId uint256 ID of the token being burned by the msg.sender
+     */
+    function _burn(address owner, uint256 tokenId) internal {
+        super._burn(owner, tokenId);
+
+        // Clear metadata (if any)
+        if (bytes(_tokenURIs[tokenId]).length != 0) {
+            delete _tokenURIs[tokenId];
+        }
+    }
 }
 
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,7 @@
+const MyTRC721 = artifacts.require('./MyTRC721');
+
+const config = require('config');
+
+module.exports = function(deployer) {
+    return deployer.deploy(MyTRC721);
+};
